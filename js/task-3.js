eacd90acce6d36f560e2e3d744d23bb5e91cf5f1
Ihor Yakovliev <ganzzzzzzz@gmail.com>
2024-09-06 18:13:01 +0100

    done

--- js/task-3.js
+++ js/task-3.js
@@ -1,20 +1,15 @@
-function checkForSpam(message) {
-    const hasSpam = message.toLowerCase();
-    if (hasSpam.includes(`spam`)) {
-        return true;
+function filterArray(numbers, value) {
+    const overArray = [];
+    for (const number of numbers) {
+        if (number > value) {
+            overArray.push(number);
+        }
     }
-    else if (hasSpam.includes(`sale`)) {
-        return true;
-    }
-    else {
-        return false;
-    }
+    return overArray;
 }
 
-console.log(checkForSpam("Latest technology news")); // false
-console.log(checkForSpam("JavaScript weekly newsletter")); // false
-console.log(checkForSpam("Get best sale offers now!")); // true
-console.log(checkForSpam("Amazing SalE, only tonight!")); // true
-console.log(checkForSpam("Trust me, this is not a spam message")); // true
-console.log(checkForSpam("Get rid of sPaM emails. Our book in on sale!")); // true
-console.log(checkForSpam("[SPAM] How to earn fast money?")); // true
+console.log(filterArray([1, 2, 3, 4, 5], 3)); // [4, 5]
+console.log(filterArray([1, 2, 3, 4, 5], 4)); // [5]
+console.log(filterArray([1, 2, 3, 4, 5], 5)); // []
+console.log(filterArray([12, 24, 8, 41, 76], 38)); // [41, 76]
+console.log(filterArray([12, 24, 8, 41, 76], 20)); // [24, 41, 76]
